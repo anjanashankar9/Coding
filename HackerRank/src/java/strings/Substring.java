3191212267a1abc9197b23cc9da281c306261ced
Anjana Shankar <anjanashankar9@gmail.com>
2019-07-16 13:10:38 +0530

    Java Substring function

--- HackerRank/src/java/strings/Substring.java
+++ HackerRank/src/java/strings/Substring.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Substring {
 
-    public static void main(String[] args) {
+    public static void substring(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
         int start = in.nextInt();
