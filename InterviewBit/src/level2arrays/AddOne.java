13af802bf2df6db53c5bf54ba99eedaf878fe350
Anjana Shankar <anjanashankar9@gmail.com>
2020-05-19 23:47:01 +0530

    Added problem statement to the code

--- InterviewBit/src/level2arrays/AddOne.java
+++ InterviewBit/src/level2arrays/AddOne.java
@@ -48,3 +48,20 @@ public class AddOne {
         System.out.println();
     }
 }
+
+/*
+Given a non-negative number represented as an array of digits,
+
+add 1 to the number ( increment the number represented by the digits ).
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+
+If the vector has [1, 2, 3]
+
+the returned vector should be [1, 2, 4]
+
+as 123 + 1 = 124.
+ */
+//https://www.interviewbit.com/problems/add-one-to-number/
