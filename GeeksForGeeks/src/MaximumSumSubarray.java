12895c4231c263846aead892213a02289f8b7002
Anjana Shankar <anjanashankar9@gmail.com>
2020-06-02 22:48:05 +0530

    Maximum Sum Subarray - prefix sum

--- GeeksForGeeks/src/MaximumSumSubarray.java
+++ GeeksForGeeks/src/MaximumSumSubarray.java
@@ -9,7 +9,7 @@ Sum = 9
 public class MaximumSumSubarray {
 
     //Brute Force - O(n*n*n)
-     static int bruteForce(int[] arr) {
+    static int bruteForce(int[] arr) {
         int n = arr.length;
         int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++) {
@@ -28,7 +28,7 @@ public class MaximumSumSubarray {
 
 
     //Almost Brute Force - O(n*n)
-     static int orderNSquare(int[] arr) {
+    static int orderNSquare(int[] arr) {
         int n = arr.length;
         int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++) {
@@ -47,29 +47,29 @@ public class MaximumSumSubarray {
     }
 
     static int maxCrossingSum(int[] arr, int l, int m, int h) {
-         int sum = 0;
-         int leftSum = Integer.MIN_VALUE;
-         for (int i=m; i>=0; i--) {
-             sum += arr[i];
-             if(sum > leftSum) {
-                 leftSum = sum;
-             }
-         }
+        int sum = 0;
+        int leftSum = Integer.MIN_VALUE;
+        for (int i=m; i>=0; i--) {
+            sum += arr[i];
+            if(sum > leftSum) {
+                leftSum = sum;
+            }
+        }
 
-         sum = 0;
-         int rightSum = Integer.MIN_VALUE;
-         for(int i=m+1; i<= h; i++) {
-             sum += arr[i];
-             if (sum > rightSum) {
-                 rightSum = sum;
-             }
-         }
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+        for(int i=m+1; i<= h; i++) {
+            sum += arr[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+            }
+        }
 
-         return Math.max(
-                 leftSum + rightSum, Math.max(
-                         leftSum, rightSum
-                 )
-         );
+        return Math.max(
+                leftSum + rightSum, Math.max(
+                        leftSum, rightSum
+                )
+        );
     }
     static int maximumSumSubarrayDivideAndConquerHelper(int[] arr, int l, int h) {
         //Base case - only one element
@@ -97,22 +97,40 @@ public class MaximumSumSubarray {
 
     // Using Kadane's Algorithm - O(n)
     static int maximumSumSubarrayKadane(int[] arr) {
-         int maxSoFar = Integer.MIN_VALUE;
-         int maxEndingHere = 0;
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndingHere = 0;
 
-         for (int i=0; i<arr.length; i++) {
-             maxEndingHere = maxEndingHere + arr[i];
-             if (maxSoFar < maxEndingHere)
-                 maxSoFar = maxEndingHere;
-             if (maxEndingHere < 0)
-                 maxEndingHere = 0;
-         }
+        for (int i=0; i<arr.length; i++) {
+            maxEndingHere = maxEndingHere + arr[i];
+            if (maxSoFar < maxEndingHere)
+                maxSoFar = maxEndingHere;
+            if (maxEndingHere < 0)
+                maxEndingHere = 0;
+        }
         return maxSoFar;
     }
 
+    // Using prefix sum
+    static int maximumSumSubarrayPrefixSum(int[] arr) {
+        int[] prefixSum = new int[arr.length];
+        prefixSum[0] = arr[0];
+        for (int i=1; i< arr.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+
+        int minPrefixSum = 0;
+        int result = Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++) {
+            result = Math.max(result, prefixSum[i] - minPrefixSum);
+            minPrefixSum = Math.min(minPrefixSum, prefixSum[i]);
+        }
+        return result;
+    }
+
     public static void maximumSumSubarray(String[] args) {
-        int []input = new int[]{-2, -3, -4, -1, -2, -1, -5, -3};
-        int result = maximumSumSubarrayKadane(input);
+        int []input = new int[]{-2, -3, 4, -1, -2, 1, 5, -3};
+        int result = maximumSumSubarrayPrefixSum(input);
         System.out.println(result);
     }
 }
