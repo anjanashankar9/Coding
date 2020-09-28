bb314f233772d3f3a67c0a41e8e911efd54eedce
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-28 22:52:13 +0530

    SLL node classes

---
+++ crackthecodinginterview/src/main/java/linkedlist/SLL.java
@@ -0,0 +1,30 @@
+package linkedlist;
+
+/**
+ * @Author Anjana Shankar
+ * @Created 2020-09-28
+ */
+class Node {
+    int data;
+    Node next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
+
+public class SLL{
+    Node head;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+}
