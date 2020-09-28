b8b43ccb66c1c861eb4b806294554b560cf11eff
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-28 19:09:01 +0530

    String Rotation

--- crackthecodinginterview/src/main/java/arraysandstrings/StringRotation.java
+++ crackthecodinginterview/src/main/java/arraysandstrings/StringRotation.java
@@ -5,8 +5,31 @@ package arraysandstrings;
  * @Created 2020-09-27
  */
 public class StringRotation {
+    /*
+    If s2 is a rotated string of s1 then that means you can represent
+    these strings as:
+    s1 = xy
+    s2 = yx
+
+    Thus, s2 will definitely be a substring of xyxy that is s1s1.
+    This is the property we will be testing.
+     */
+    private boolean isRotatedString(String s1, String s2) {
+        String s = new String(s1+s1);
+        return s.contains(s2);
+    }
+
+    public static void main(String args[]) {
+        String s1 = "waterbottle";
+        String s2 = "erbottlewat";
+        System.out.println(new StringRotation().isRotatedString(s1, s2));
+    }
 }
 
 /*
-Assumeyou have a method isSubstringwhich checks if one word is a substring of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
+Assumeyou have a method isSubstring which checks if one word
+is a substring of another.
+Given two strings, sl and s2, write code to check if s2 is a
+rotation of sl using only one call to
+isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
  */
