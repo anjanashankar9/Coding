776902acb72c4f8fd86f602979e1aa0f0fcebdd5
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-28 19:34:52 +0530

    String Compression

--- crackthecodinginterview/src/main/java/arraysandstrings/StringCompression.java
+++ crackthecodinginterview/src/main/java/arraysandstrings/StringCompression.java
@@ -5,7 +5,32 @@ package arraysandstrings;
  * @Created 2020-09-27
  */
 public class StringCompression {
+    private String compress(String input) {
+        StringBuffer s = new StringBuffer();
+        char[] characterArray = input.toCharArray();
+        char prev = characterArray[0];
+        s.append(prev);
+        int count = 1;
+        for(int i=1; i< input.length(); i++) {
+            char curr = characterArray[i];
+            if (curr == prev) {
+                count++;
+            }
+            else {
+                s.append(count);
+                count = 1;
+                s.append(curr);
+            }
+            prev = curr;
+        }
+        s.append(count);
 
+        return s.toString().length() < input.length() ? s.toString() : input;
+    }
+    public static void stringCompression(String[] args) {
+        String s1 = "aabcccccaaa";
+        System.out.println(new StringCompression().compress(s1));
+    }
 }
 
 /*
