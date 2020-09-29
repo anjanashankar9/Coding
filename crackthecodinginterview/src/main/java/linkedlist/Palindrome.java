22d9a768afde5a58e1b7a5a4d8d974aab02b74d9
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-29 19:51:08 +0530

    Palindrome

--- crackthecodinginterview/src/main/java/linkedlist/Palindrome.java
+++ crackthecodinginterview/src/main/java/linkedlist/Palindrome.java
@@ -1,10 +1,38 @@
 package linkedlist;
 
+import java.util.Stack;
+
 /**
  * @Author Anjana Shankar
  * @Created 2020-09-28
  */
 public class Palindrome {
+    boolean isPalindrome(Node head) {
+        Stack<Integer> stack = new Stack<>();
+
+        Node slowPtr = head;
+        Node fastPtr = head;
+
+        while(fastPtr != null && fastPtr.next != null) {
+            stack.push(slowPtr.data);
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+
+        // Skipping the middle element for an odd length list
+        if(fastPtr != null) {
+            slowPtr = slowPtr.next;
+        }
+
+        while(slowPtr != null) {
+            int top = stack.pop().intValue();
+            if(top != slowPtr.data) {
+                return false;
+            }
+            slowPtr = slowPtr.next;
+        }
+        return true;
+    }
 }
 
 /*
