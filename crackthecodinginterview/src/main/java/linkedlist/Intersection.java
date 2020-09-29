eea571c0395f7410dc9895272b6b16f104cd4d74
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-29 19:26:06 +0530

    Intersection

--- crackthecodinginterview/src/main/java/linkedlist/Intersection.java
+++ crackthecodinginterview/src/main/java/linkedlist/Intersection.java
@@ -5,6 +5,57 @@ package linkedlist;
  * @Created 2020-09-28
  */
 public class Intersection {
+    /**
+     * Finds the intersection point of the two Singly Linked Lists
+     * @param head1
+     * @param head2
+     * @return the intersecting Node
+     */
+    private Node intersection(Node head1, Node head2) {
+        if(head1 == null || head2 == null) {
+            return null;
+        }
+        int l1 = 1;
+        int l2 = 1;
+
+        Node temp1 = head1;
+        while(temp1.next != null) {
+            l1++;
+            temp1 = temp1.next;
+        }
+
+        Node temp2 = head2;
+        while(temp2.next != null) {
+            l2++;
+            temp2 = temp2.next;
+        }
+
+        if(temp1 != temp2) {
+            System.out.println("The lists do not intersect");
+            return null;
+        }
+
+        int k = Math.abs(l1-l2);
+        temp1 = head1;
+        temp2 = head2;
+        if(l1 > l2) {
+            for(int i=0; i<k; i++) {
+                temp1 = temp1.next;
+            }
+        }
+
+        if(l2 > l1) {
+            for(int i=0; i<k; i++) {
+                temp2 = temp2.next;
+            }
+        }
+
+        while(temp1 != temp2) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return temp1;
+    }
 }
 
 /*
