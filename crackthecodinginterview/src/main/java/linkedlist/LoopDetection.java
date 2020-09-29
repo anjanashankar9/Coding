9356353bdcfb7e44fd21edaf3d7a1ef0cd576891
Anjana Shankar <anjanashankar9@gmail.com>
2020-09-29 11:30:51 +0530

    Loop Detection

--- crackthecodinginterview/src/main/java/linkedlist/LoopDetection.java
+++ crackthecodinginterview/src/main/java/linkedlist/LoopDetection.java
@@ -5,6 +5,37 @@ package linkedlist;
  * @Created 2020-09-28
  */
 public class LoopDetection {
+    private Node loopDetect(Node head) {
+        Node temp1 = head;
+        Node temp2 = head;
+
+        while(temp2 != null && temp2.next != null) {
+            temp1 = temp1.next;
+            temp2 = temp2.next.next;
+
+            if(temp1 == temp2) {
+                //Found the collision point. Break from here.
+                break;
+            }
+        }
+
+        if(temp2 == null || temp2.next == null) {
+            //No loop
+            return null;
+        }
+
+        // Move the slow pointer to head of  linked list.
+        temp1 = head;
+        while(temp1 != temp2) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        //They are not at the start of the loop
+        return temp1;
+    }
+    public static void loopDetect(String[] args) {
+
+    }
 }
 
 /*
@@ -15,7 +46,7 @@ Circular linked list: A (corrupt) linked list in
 which a node's next pointer points to an earlier node, so as
 to make a loop in the linked list.
 EXAMPLE
-Input: A -> B -> C -> D -> E -> C[thesameCasearlier]
+Input: A -> B -> C -> D -> E -> C[the same C as earlier]
 Output: C
 
  */
