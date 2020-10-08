1dfe3f1f94e86c9041b1626582357c64ce9c1d53
Anjana Shankar <anjanashankar9@gmail.com>
2020-10-08 18:48:14 +0530

    Deadlock

---
+++ miscellaneous/src/main/java/deadlock/DeadlockExample1.java
@@ -0,0 +1,50 @@
+package deadlock;
+
+/**
+ * @Author Anjana Shankar
+ * @Created 2020-10-07
+ */
+public class DeadlockExample1 {
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread() {
+            public void run() {
+                synchronized (lock1) {
+                    System.out.println("Thread 1: Acquired lock1");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    System.out.println("Thread 1: Waiting for lock2...");
+                    synchronized (lock2) {
+                        System.out.println("Thread 1: Holding both lock1 and lock2");
+                    }
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                synchronized (lock2) {
+                    System.out.println("Thread 2: Acquired lock2");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    System.out.println("Thread 2: Waiting for lock1...");
+                    synchronized (lock1) {
+                        System.out.println("Thread 2: Holding both lock2 and lock1");
+                    }
+                }
+            }
+        };
+
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
