70d4b8408a1553fc5ad1a570503f2e239a00f946
Anjana Shankar <anjanashankar9@gmail.com>
2020-11-22 19:20:47 +0530

    Small fix

--- leetcode/algorithms/src/main/java/medium/LongestIncreasingSubsequence.java
+++ leetcode/algorithms/src/main/java/medium/LongestIncreasingSubsequence.java
@@ -28,7 +28,7 @@ public class LongestIncreasingSubsequence {
          */
 
         int[] dp = new int[nums.length];
-        for(int i=0; i<nums.length-1; i++) {
+        for(int i=0; i<nums.length; i++) {
             dp[i] = 1;
         }
 
