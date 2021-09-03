a34d82bc45ba57b03549c767e2037cd196395f9e
Anjana Shankar <anjana.shankar@salesforce.com>
2021-09-03 09:59:54 +0530

    Symmetric Tree and Intersection of Two Arrays

---
+++ leetcode/algorithms/src/main/java/easy/IntersectionTwoArrays.java
@@ -0,0 +1,50 @@
+package easy;
+
+import java.util.*;
+
+public class IntersectionTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+
+        for(int j=0; j<nums2.length; j++) {
+            if(set.contains(nums2[j])) {
+                result.add(nums2[j]);
+                set.remove(nums2[j]);
+            }
+        }
+        int[] res = new int[result.size()];
+        for (int k=0; k<result.size(); k++)
+            res[k] = result.get(k);
+        return res;
+    }
+}
+
+
+
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+ */
--- leetcode/algorithms/src/main/java/easy/SymmetricTree.java
+++ leetcode/algorithms/src/main/java/easy/SymmetricTree.java
@@ -27,7 +27,9 @@ public class SymmetricTree {
 /*
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
-
+                 1
+            2          2
+        3      4    4     3
 
 Example 1:
 
